212688217807aefb50ba9646d19e6c240832772a
Rahulkumar <rahgadda@cisco.com>
2025-06-08 17:30:41 -0700

    Add ReadabilityTuner and case study formatter

--- BookLLM/src/agents/review/__init__.py
+++ BookLLM/src/agents/review/__init__.py
@@ -1,4 +1,5 @@
 from .proofreader import ProofreaderAgent
+from .readability_tuner import ReadabilityTuner
 from .validator import ContentValidator
 
-__all__ = ["ProofreaderAgent", "ContentValidator"]
+__all__ = ["ProofreaderAgent", "ContentValidator", "ReadabilityTuner"]
